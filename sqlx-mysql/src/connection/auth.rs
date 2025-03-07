b67919e820bb848bdc15363edee7371c98b11c3d
Arthur Gautier <arthur.gautier@arista.com>
2025-03-06 22:52:27 -0800

    bump rustcrypto to pre-releases

--- sqlx-mysql/src/connection/auth.rs
+++ sqlx-mysql/src/connection/auth.rs
@@ -1,7 +1,6 @@
 use bytes::buf::Chain;
 use bytes::Bytes;
-use digest::{Digest, OutputSizeUser};
-use generic_array::GenericArray;
+use digest::{array::Array, Digest, OutputSizeUser};
 use rand::thread_rng;
 use rsa::{pkcs8::DecodePublicKey, Oaep, RsaPublicKey};
 use sha1::Sha1;
@@ -77,7 +76,7 @@ impl AuthPlugin {
 fn scramble_sha1(
     password: &str,
     nonce: &Chain<Bytes, Bytes>,
-) -> GenericArray<u8, <Sha1 as OutputSizeUser>::OutputSize> {
+) -> Array<u8, <Sha1 as OutputSizeUser>::OutputSize> {
     // SHA1( password ) ^ SHA1( seed + SHA1( SHA1( password ) ) )
     // https://mariadb.com/kb/en/connection/#mysql_native_password-plugin
 
@@ -105,7 +104,7 @@ fn scramble_sha1(
 fn scramble_sha256(
     password: &str,
     nonce: &Chain<Bytes, Bytes>,
-) -> GenericArray<u8, <Sha256 as OutputSizeUser>::OutputSize> {
+) -> Array<u8, <Sha256 as OutputSizeUser>::OutputSize> {
     // XOR(SHA256(password), SHA256(seed, SHA256(SHA256(password))))
     // https://mariadb.com/kb/en/caching_sha2_password-authentication-plugin/#sha-2-encrypted-password
     let mut ctx = Sha256::new();
